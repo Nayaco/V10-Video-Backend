4adfcb2819b4edc9a432d27ac3b53812d58fc866
NyancoChan <nayacooo@gmail.com>
2018-09-23 10:48:00 +0800

    auto commit

--- App/controllers/index.ts
+++ App/controllers/index.ts
@@ -3,8 +3,10 @@ import * as Router from 'koa-router';
 import {upload, reg, del, cancel, stop, verify} from './upload';
 const _router = new Router();
 
+/// main entry of this koa-application
 export const router = ():Koa.Middleware=> {
     _router.post('/media',upload);
     _router.post('/regmedia', reg);
+    _router
     return _router.routes();
 }
