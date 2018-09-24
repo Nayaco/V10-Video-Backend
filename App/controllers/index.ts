cb6ddddf950a4ff6abac7380cd2600e085fa3bee
NyancoChan <nayacooo@gmail.com>
2018-09-24 14:15:56 +0800

    auto commit

--- App/controllers/index.ts
+++ App/controllers/index.ts
@@ -4,6 +4,11 @@ import {upload, reg, del, cancel, stop, verify} from './upload';
 const _router = new Router();
 
 /// main entry of this koa-application
+/// added following controlllers
+/// 1. media upload router
+/// 2. media regester
+/// 3. media delete
+/// 
 export const router = ():Koa.Middleware=> {
     _router.post('/media',upload);
     _router.post('/regmedia', reg);
